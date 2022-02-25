cc5dd45f573a11c53f4d0663e2083eae46d71d63
Viacheslav-Terentyev <terentyev.va@yandex.ru>
2022-02-25 12:37:08 +0300

    task completed

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -17,7 +17,25 @@ class ProfileView: UIView {
     @IBOutlet weak var userInfoText: UITextView!
     
     
-    
-    
+   
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            self.setupView()
+        }
+        required init?(coder: NSCoder) {
+            super.init(coder: coder)
+            self.setupView()
+        }
+        private func setupView() {
+            let view = self.loadViewFromXib()
+            self.addSubview(view)
+            view.frame = self.bounds
+            view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        }
+        private func loadViewFromXib() -> UIView {
+            guard let view = Bundle.main.loadNibNamed("ProfileView", owner: self, options: nil)?.first as? UIView else { return UIView() }
+            
+            return view
+        }
     
 }
---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Вячеслав Терентьев on 25.02.2022.
+//
+
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+            super.viewDidLoad()
+ 
+    }
+}
