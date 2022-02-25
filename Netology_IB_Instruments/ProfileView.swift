bd2524eb4ac480633d2a83afe05446cfb700a69a
Viacheslav-Terentyev <terentyev.va@yandex.ru>
2022-02-25 09:40:17 +0300

    add ui components

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Вячеслав Терентьев on 25.02.2022.
+//
+
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var userPhotoImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userDataOfDate: UILabel!
+    @IBOutlet weak var userCityLabel: UILabel!
+    @IBOutlet weak var userInfoText: UITextView!
+    
+    
+    
+    
+    
+}
